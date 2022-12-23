1f91e6231e625a2cb49294cee709fff5c42c5d7f
Zub04ek <nastasyz@ukr.net>
2022-12-23 12:03:51 +0200

    add 2-3 completed tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,30 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-console.log(Player);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+
+const currentSeconds = localStorage.getItem('videoplayer-current-time');
+const parsedCurrentSeconds = JSON.parse(currentSeconds) || 0;
+
+player
+  .setCurrentTime(parsedCurrentSeconds)
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(error.message);
+        break;
+
+      default:
+        console.log(error.message);
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,35 @@
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
-console.log(throttle);
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('input');
+const textareaRef = document.querySelector('textarea');
+
+getUserData();
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+const formData = {};
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  formData[inputRef.name] = inputRef.value;
+  formData[textareaRef.name] = textareaRef.value;
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function getUserData() {
+  const userData = localStorage.getItem('feedback-form-state');
+  if (userData) {
+    const parsedUserData = JSON.parse(userData);
+    inputRef.value = parsedUserData.email;
+    textareaRef.value = parsedUserData.message;
+  }
+}
